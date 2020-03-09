49064d33138b449c53deb142da42681071d651a9
DY <ehdudakswo@gmail.com>
2020-03-09 17:27:32 +0900

    Create Android Project (MemoryGod)

---
+++ MemoryGod/app/src/main/java/com/dy/memorygod/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dy.memorygod
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
