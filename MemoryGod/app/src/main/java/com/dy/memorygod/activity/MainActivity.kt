b7087eae4ecce36960caa7c963a465b4c8d5ab20
DY <ehdudakswo@gmail.com>
2020-03-21 13:08:58 +0900

    Refactoring (Permission in TestActivity)

--- MemoryGod/app/src/main/java/com/dy/memorygod/activity/MainActivity.kt
+++ MemoryGod/app/src/main/java/com/dy/memorygod/activity/MainActivity.kt
@@ -7,22 +7,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dy.memorygod.R
 import com.dy.memorygod.adapter.MainRecyclerViewAdapter
-import com.dy.memorygod.data.ContactEmailData
-import com.dy.memorygod.data.ContactPhoneNumberData
 import com.dy.memorygod.data.MainData
 import com.dy.memorygod.data.MainDataContent
 import com.dy.memorygod.enums.IntentName
 import com.dy.memorygod.enums.PreferenceKey
-import com.dy.memorygod.manager.ContactManager
 import com.dy.memorygod.manager.JsonManager
 import com.dy.memorygod.manager.MainDataManager
 import com.dy.memorygod.manager.PreferenceManager
-import com.gun0912.tedpermission.PermissionListener
-import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
-import java.text.SimpleDateFormat
-import java.util.*
-import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,44 +24,68 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        checkPermissions()
+        MainDataManager.init()
+        setDefaultList()
+        loadBackup()
+        setRecyclerView()
+        refreshRecyclerView()
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
-        setBackup()
+        saveBackup()
     }
 
-    private fun checkPermissions() {
-        TedPermission.with(this)
-            .setPermissionListener(permissionListener)
-            .setPermissions(
-                android.Manifest.permission.READ_CONTACTS
-            )
-            .check()
+    private fun setDefaultList() {
+        setPhoneNumber()
+        setWordSample()
     }
 
-    private val permissionListener: PermissionListener =
-        object : PermissionListener {
-            override fun onPermissionGranted() {
-                MainDataManager.init()
-                setRecyclerView()
-                setContactList()
-                setTestList()
-                refreshBackup()
-                refreshRecyclerView()
-            }
+    private fun setPhoneNumber() {
+        val subject = getString(R.string.app_main_phoneNumber_subject)
+        val contentList = ArrayList<MainDataContent>()
 
-            override fun onPermissionDenied(deniedPermissions: ArrayList<String>?) {
-                Toast.makeText(
-                        this@MainActivity,
-                        R.string.app_permission_need, Toast.LENGTH_LONG
-                    )
-                    .show()
-                finish()
-            }
+        val data = MainData(subject, contentList, true)
+        MainDataManager.dataList.add(data)
+    }
+
+    private fun setWordSample() {
+        val subject = getString(R.string.app_main_wordSample_subject)
+        val contentList = ArrayList<MainDataContent>()
+
+        contentList.add(MainDataContent("사과", "apple"))
+        contentList.add(MainDataContent("한국", "korea"))
+
+        val data = MainData(subject, contentList, false)
+        MainDataManager.dataList.add(data)
+    }
+
+    private fun saveBackup() {
+        val backupDataList = MainDataManager.getBackupDataList()
+        val key = PreferenceKey.MainBackupDataList.toString()
+        val value = JsonManager.toJson(backupDataList)
+
+        PreferenceManager.set(this, key, value)
+
+        val msg = buildString { backupDataList.forEach { appendln(it.subject) } }
+        Toast.makeText(this, "saveBackup\n$msg", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun loadBackup() {
+        val key = PreferenceKey.MainBackupDataList.toString()
+        val value = PreferenceManager.get(this, key)
+
+        if (value == PreferenceManager.PREFERENCE_DEFAULT_VALUE) {
+            return
         }
+
+        val backupDataList = JsonManager.toMainBackupDataList(value)
+        MainDataManager.refreshBackup(backupDataList)
+
+        val msg = buildString { MainDataManager.dataList.forEach { appendln(it.subject) } }
+        Toast.makeText(this, "loadBackup\n$msg", Toast.LENGTH_SHORT).show()
+    }
 
     private fun setRecyclerView() {
         val recyclerView = recyclerView_main
@@ -83,16 +99,8 @@ class MainActivity : AppCompatActivity() {
             MainRecyclerViewAdapter.ItemClickListener {
             override fun onClick() {
                 val selectedItem = recyclerViewAdapter.selectedItem
-                if (selectedItem.contentList.isEmpty()) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        R.string.app_main_contentList_empty,
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    return
-                }
-
                 MainDataManager.selectedData = selectedItem
+
                 val intent = Intent(this@MainActivity, TestActivity::class.java)
                 val intentName = IntentName.TestConfig.toString()
                 val config = selectedItem.subject
@@ -103,92 +111,9 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun setContactList() {
-        val phoneNumberList = ContactManager.getPhoneNumberList(this)
-        val emailList = ContactManager.getEmailList(this)
-
-        if (phoneNumberList == ContactManager.ERROR_CONTACT_PHONE_NUMBER) {
-            Toast.makeText(this, ContactManager.ERROR_MESSAGE, Toast.LENGTH_LONG).show()
-            finish()
-            return
-        }
-
-        setPhoneNumberList(phoneNumberList)
-        setEmailList(emailList)
-    }
-
-    private fun setPhoneNumberList(phoneNumberList: List<ContactPhoneNumberData>) {
-        val subject = getString(R.string.app_main_phoneNumber_subject)
-        val contentList = ArrayList<MainDataContent>()
-
-        for (phoneNumber in phoneNumberList) {
-            val title = phoneNumber.name
-            val data = phoneNumber.phoneNumber
-
-            val content = MainDataContent(title, data)
-            contentList.add(content)
-        }
-
-        val data = MainData(subject, contentList, true)
-        MainDataManager.dataList.add(data)
-    }
-
-    private fun setEmailList(emailList: List<ContactEmailData>) {
-        val subject = getString(R.string.app_main_email_subject)
-        val contentList = ArrayList<MainDataContent>()
-
-        for (email in emailList) {
-            val title = email.name
-            val data = email.email
-
-            val content = MainDataContent(title, data)
-            contentList.add(content)
-        }
-
-        val data = MainData(subject, contentList, true)
-        MainDataManager.dataList.add(data)
-    }
-
-    private fun setTestList() {
-        val contentList = ArrayList<MainDataContent>()
-
-        val date1 = SimpleDateFormat("yyyy-MM-dd").format(Date())
-        val date2 = SimpleDateFormat("HH:mm:ss").format(Date())
-        contentList.add(MainDataContent(date1, date2))
-
-        val data = MainData("SubjectTest", contentList, false)
-        MainDataManager.dataList.add(data)
-    }
-
-    private fun setBackup() {
-        val backupDataList = MainDataManager.getBackupDataList()
-        val key = PreferenceKey.MainBackupDataList.toString()
-        val value = JsonManager.toJson(backupDataList)
-
-        PreferenceManager.set(this, key, value)
-
-        val msg = buildString { backupDataList.forEach { appendln(it.subject) } }
-        Toast.makeText(this, "backupDataList\n$msg", Toast.LENGTH_SHORT).show()
-    }
-
-    private fun refreshBackup() {
-        val key = PreferenceKey.MainBackupDataList.toString()
-        val value = PreferenceManager.get(this, key)
-
-        if (value == PreferenceManager.PREFERENCE_DEFAULT_VALUE) {
-            return
-        }
-
-        val backupDataList = JsonManager.toMainBackupDataList(value)
-        MainDataManager.refreshBackup(backupDataList)
-    }
-
     private fun refreshRecyclerView() {
         val mainDataList = MainDataManager.dataList
         recyclerViewAdapter.refresh(mainDataList)
-
-        val msg = buildString { mainDataList.forEach { appendln(it.subject) } }
-        Toast.makeText(this, "mainDataList\n$msg", Toast.LENGTH_SHORT).show()
     }
 
 }
--- MemoryGod/app/src/main/java/com/dy/memorygod/activity/TestActivity.kt
+++ MemoryGod/app/src/main/java/com/dy/memorygod/activity/TestActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.dy.memorygod.R
+import com.dy.memorygod.data.MainDataContent
 import com.dy.memorygod.enums.IntentName
+import com.dy.memorygod.manager.ContactManager
 import com.dy.memorygod.manager.MainDataManager
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 
 class TestActivity : AppCompatActivity() {
+
+    private val selectedData = MainDataManager.selectedData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,20 +22,84 @@ class TestActivity : AppCompatActivity() {
         val intentName = IntentName.TestConfig.toString()
         val config = intent.getStringExtra(intentName)
 
-        val selectedData = MainDataManager.selectedData
+        checkDataType()
+    }
+
+    private fun checkDataType() {
         if (selectedData.isPhoneData) {
-            Toast.makeText(
-                this,
-                selectedData.toString(),
-                Toast.LENGTH_SHORT
-            ).show()
+            checkPermissions()
         } else {
-            Toast.makeText(
-                this,
-                "Writable",
-                Toast.LENGTH_SHORT
-            ).show()
+            initActivity()
         }
+    }
+
+    private fun checkPermissions() {
+        TedPermission.with(this)
+            .setPermissionListener(permissionListener)
+            .setPermissions(
+                android.Manifest.permission.READ_CONTACTS
+            )
+            .check()
+    }
+
+    private val permissionListener: PermissionListener =
+        object : PermissionListener {
+            override fun onPermissionGranted() {
+                if (handlePhoneData()) {
+                    initActivity()
+                }
+            }
+
+            override fun onPermissionDenied(deniedPermissions: ArrayList<String>?) {
+                Toast.makeText(
+                        this@TestActivity,
+                        R.string.app_permission_need, Toast.LENGTH_LONG
+                    )
+                    .show()
+                finish()
+            }
+        }
+
+    private fun handlePhoneData(): Boolean {
+        when (selectedData.subject) {
+            getString(R.string.app_main_phoneNumber_subject) -> return setPhoneNumberList()
+        }
+
+        return false
+    }
+
+    private fun setPhoneNumberList(): Boolean {
+        val phoneNumberList = ContactManager.getPhoneNumberList(this)
+        if (phoneNumberList == ContactManager.ERROR_CONTACT_PHONE_NUMBER) {
+            Toast.makeText(this, ContactManager.ERROR_MESSAGE, Toast.LENGTH_LONG).show()
+            finish()
+            return false
+        }
+
+        val contentList = selectedData.contentList
+        contentList.clear()
+
+        for (phoneNumber in phoneNumberList) {
+            val title = phoneNumber.name
+            val data = phoneNumber.phoneNumber
+
+            val content = MainDataContent(title, data)
+            contentList.add(content)
+        }
+
+        return true
+    }
+
+    private fun initActivity() {
+        val subject = selectedData.subject
+        val contentList = selectedData.contentList
+        val msg = "$subject (${contentList.size})\n$contentList"
+
+        Toast.makeText(
+            this,
+            msg,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
 }
