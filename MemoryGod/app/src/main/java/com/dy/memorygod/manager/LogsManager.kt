9d17b33af061e9529c119476e4db8f20f2118149
ehdudakswo-gmail <ehdudakswo@gmail.com>
2021-01-06 16:53:36 +0900

    LogsManager 추가

---
+++ MemoryGod/app/src/main/java/com/dy/memorygod/manager/LogsManager.kt
@@ -0,0 +1,13 @@
+package com.dy.memorygod.manager
+
+import android.util.Log
+
+object LogsManager {
+
+    private const val TAG = "ehdudakswo"
+
+    fun d(message: String) {
+        Log.d(TAG, message)
+    }
+
+}
